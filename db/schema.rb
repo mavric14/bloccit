1196dcd15699f90db9fd47aa0aa89da784878821
Marc <valente.marc@gmail.com>
2014-04-23 20:07:45 -0700

    Implemented Pundit and basic roles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423003909) do
+ActiveRecord::Schema.define(version: 20140423045909) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20140423003909) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
