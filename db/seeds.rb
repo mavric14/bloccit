df1864f6d60f5ebd102568f23d3c143a5634562d
Marc <valente.marc@gmail.com>
2014-05-03 23:36:53 -0700

    Implemented voting

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,15 @@ end
 topics = Topic.all
 
 # Create Posts
-100.times do 
-  Post.create(
+50.times do 
+  post = Post.create(
     user:   users.sample,
     topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  post.update_rank
 end
 
 posts = Post.all
