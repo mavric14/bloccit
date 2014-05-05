10f4d10dc82283d1d22bb33c70ecef607b93096e
Marc <valente.marc@gmail.com>
2014-05-05 10:45:33 -0700

    Implemented favoriting

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,14 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   validates :body, length: { minimum: 5}, presence: true
+
+  after_create :send_favorite_emails
+
+  private
+
+  def send_favorite_emails
+    self.post.favorites.each do |favorite|
+      FavoriteMailsr.new_comment(favorite.user, self.post, self).deliver
+  end
+end
 end
